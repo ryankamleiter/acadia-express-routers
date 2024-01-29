637926a6bdc62087ce2cf8d8b011c64f1936a88a
Ryan Kamleiter <kamleiterryan@gmail.com>
2024-01-29 16:58:40 -0600

    bugs have been squashed

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bookRouter = require('./routes/books.router.js');
+const movieRouter = require('./routes/movies.router.js');
 
 const app = express();
 
@@ -17,16 +18,7 @@ app.use(express.static('server/public'));
 
 // TODO - Make sure to move these bits of data
 //        into their corresponding router files!
-const movies = [
-  {
-    title: 'Lady Bird',
-    director: 'Greta Gerwig'
-  },
-  {
-    title: 'Everything Everywhere All at Once',
-    director: 'Daniel Kwan and Daniel Scheinert'
-  }
-];
+
 
 /** ---------- EXPRESS ROUTES ---------- **/
 
@@ -36,19 +28,8 @@ const movies = [
 // The second thing, is the router
 app.use('/books', bookRouter);
 
+app.use('/movies', movieRouter);
 
-app.get('/movies', (req, res) => {
-  console.log('GET /movies received a request!')
-  res.send(movies);
-});
-
-app.post('/movies', (req, res) => {
-  console.log('POST /movies received a request!')
-  console.log('req.body:', req.body)
-
-  movies.push(req.body);
-  res.sendStatus(200);
-});
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
